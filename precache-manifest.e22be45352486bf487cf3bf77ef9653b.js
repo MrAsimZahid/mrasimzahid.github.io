25b444e0a7fe1cb959ff49cd925eb0386a59a04c
MrAsimZahid <asimzahid02@gmail.com>
2021-09-20 14:56:23 +0500

    Updates

---
+++ precache-manifest.e22be45352486bf487cf3bf77ef9653b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c574efb755b6dc0496b98163ddb50fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67634dc56d0ed225b7aa",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "163c4fa53e2fe95c1241",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "bda650b02da20e220449",
+    "url": "/static/css/main.f915534d.chunk.css"
+  },
+  {
+    "revision": "9d177e1299520cd9926d",
+    "url": "/static/js/2.8915920a.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.8915920a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67634dc56d0ed225b7aa",
+    "url": "/static/js/3.c84b0e4d.chunk.js"
+  },
+  {
+    "revision": "163c4fa53e2fe95c1241",
+    "url": "/static/js/4.63d6da99.chunk.js"
+  },
+  {
+    "revision": "bda650b02da20e220449",
+    "url": "/static/js/main.58f3bb0e.chunk.js"
+  },
+  {
+    "revision": "30c34bfa9a5bf84142f4",
+    "url": "/static/js/runtime-main.9e4c3075.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "93b92b233c8c060b5afe38d357aacc3c",
+    "url": "/static/media/aws.93b92b23.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "40f3a30f64c7002a1e9506560663503f",
+    "url": "/static/media/datacamp.40f3a30f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e06e1168d5596e0494165f241b0236cf",
+    "url": "/static/media/gufhtugu.e06e1168.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8330709c284142d9eb224462e595adcc",
+    "url": "/static/media/milestonezero.8330709c.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "afd284726ddf5fbf79da79927cfd026a",
+    "url": "/static/media/zameen-logo.afd28472.jpg"
+  }
+]);
